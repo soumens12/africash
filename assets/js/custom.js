8fd368e11edf8947a2cad5e953c5ddefa7b925ad
Soumen Sarkar <soumens180@gmail.com>
2026-02-05 22:35:59 +0530

    gsap animation added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -220,29 +220,29 @@
   } catch {}
 
   // Images Animation JS
-  //   gsap.registerPlugin(ScrollTrigger);
-  //   let revealContainers = document.querySelectorAll(".reveal");
-  //   revealContainers.forEach((container) => {
-  //     let image = container.querySelector("img");
-  //     let tl = gsap.timeline({
-  //       scrollTrigger: {
-  //         trigger: container,
-  //         toggleActions: "restart none none reset",
-  //       },
-  //     });
+  gsap.registerPlugin(ScrollTrigger);
+  let revealContainers = document.querySelectorAll(".reveal");
+  revealContainers.forEach((container) => {
+    let image = container.querySelector("img");
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        toggleActions: "restart none none reset",
+      },
+    });
 
-  //     tl.set(container, { autoAlpha: 1 });
-  //     tl.from(container, 1.5, {
-  //       xPercent: -100,
-  //       ease: Power2.out,
-  //     });
-  //     tl.from(image, 1.5, {
-  //       xPercent: 100,
-  //       scale: 1.3,
-  //       delay: -1.5,
-  //       ease: Power2.out,
-  //     });
-  //   });
+    tl.set(container, { autoAlpha: 1 });
+    tl.from(container, 1.5, {
+      xPercent: -100,
+      ease: Power2.out,
+    });
+    tl.from(image, 1.5, {
+      xPercent: 100,
+      scale: 1.3,
+      delay: -1.5,
+      ease: Power2.out,
+    });
+  });
 
   // Odometer JS
   if ("IntersectionObserver" in window) {
